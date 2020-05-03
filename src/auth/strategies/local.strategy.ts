9623af8e16f41274c7abf96dea62682d1eed3b0c
LeoBergerot <leo.bergerot@gmail.com>
2020-05-03 22:59:35 +0200

    fix some problems in production

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
-import { ValidationArguments, Validator } from 'class-validator';
+import { isNotEmpty, ValidationArguments } from 'class-validator';
 import { isTokenValidConstraint } from '../recaptcha-token-constraint';
 
 @Injectable()
@@ -12,8 +12,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(req, username: string, password: string): Promise<any> {
-    const validator = new Validator();
-    if (!validator.isNotEmpty(req.body.recaptchaToken) || !await (new isTokenValidConstraint).validate(req.body.recaptchaToken, new class implements ValidationArguments {
+    if (isNotEmpty(req.body.recaptchaToken) || !await (new isTokenValidConstraint).validate(req.body.recaptchaToken, new class implements ValidationArguments {
       constraints: any[];
       object: Object;
       property: string;
